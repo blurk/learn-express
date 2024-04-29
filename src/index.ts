a79918e775bc18d6d04d56c61fc5abda150989eb
blurk <nsinh6745@gmail.com>
2024-04-29 19:06:19 +0700

    init code

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express()
+
+const PORT = 3000
+
+app.get('/', (req, res) => {
+	res.send('<h1>Hello</h1>')
+})
+
+app.listen(PORT, () => {
+	console.log('Server listen on port: ' + PORT)
+})
