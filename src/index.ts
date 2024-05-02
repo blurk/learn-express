273bc3a0fb25306f85306ca73c9a0a147cd429a6
blurk <nsinh6745@gmail.com>
2024-05-02 15:10:44 +0700

    feat: http server

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
-import express from "express"
-
-const app = express()
+import http from 'node:http'
+import fs from 'node:fs/promises'
 
 const PORT = 3000
 
-app.get('/', (req, res) => {
-	res.send('<h1>Hello</h1>')
-})
 
-app.listen(PORT, () => {
-	console.log('Server listen on port: ' + PORT)
-})
+const server = http.createServer()
+
+server.on('request', async (req, res) => {
+
+	if (req.method === 'GET') {
+		res.writeHead(200, {
+			'Content-Type': 'text/html',
+		});
+
+		try {
+			const fileName = req.url === '/' ? '/index' : req.url
+			const data = await fs.readFile(__dirname + fileName + '.html', { encoding: 'utf8' })
+			res.write(data)
+		} catch (error) {
+			const data = await fs.readFile(__dirname + '/404.html', { encoding: 'utf8' })
+			res.write(data)
+		}
+
+		res.end()
+	}
+}).listen(PORT)
